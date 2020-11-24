3eb16b1d99aa3c30de08f6d31e4299e8a835081f
Florent Lassalle <fl4@sanger.ac.uk>
2020-11-24 10:47:12 +0000

    add lsfullpath.py from pantagruel/scripts

---
+++ misc/lsfullpath.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+import sys
+import glob
+import os
+
+for target in sys.argv[1:]:
+	if (('*' in target) or (('[' in target.replace('\[', '')) and (']' in target.replace('\]', '')))):
+		lpath = glob.iglob(target)
+	elif os.path.isdir(target):
+		lpath = [os.path.join(target, nf) for nf in os.listdir(target)]
+	else:
+		lpath = [target]
+	for f in lpath:
+		print f
